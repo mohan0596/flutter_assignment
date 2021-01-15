75b9c5f6fce2d2903e3ad4d2938d51c2ab7d7507
mohan <mohan.singh@neosofttech.com>
2021-01-15 19:32:20 +0530

    Form add screen

--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -1,34 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/ui/placeholderwidget.dart';
 
+import 'formadd/form_add_screen.dart';
+
+GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Dashboard"),
-          backgroundColor: Colors.blueAccent,
+      appBar: AppBar(
+        title: Text(
+          "Dashboard",
+          style: TextStyle(
+            color: Colors.blueAccent,
+          ),
         ),
+        actions: <Widget>[
+          GestureDetector(
+            onTap: ()=> {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return FormAddScreen();
+                }),
+              )
+
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
-            ListTile(title: Text("Home"),),
-            ListTile(title: Text("My Wallet"),),
-            ListTile(title: Text("Redeem"),),
-            ListTile(title: Text("Offers"),),
-            ListTile(title: Text("10 + 1 plan"),),
-            ListTile(title: Text("My Gold Mine"),),
-            ListTile(title: Text("FAQ"),),
-            ListTile(title: Text("Logout"),),
+            ListTile(
+              title: Text("Home"),
+            ),
+            ListTile(
+              title: Text("My Wallet"),
+            ),
+            ListTile(
+              title: Text("Redeem"),
+            ),
+            ListTile(
+              title: Text("Offers"),
+            ),
+            ListTile(
+              title: Text("10 + 1 plan"),
+            ),
+            ListTile(
+              title: Text("My Gold Mine"),
+            ),
+            ListTile(
+              title: Text("FAQ"),
+            ),
+            ListTile(
+              title: Text("Logout"),
+            ),
           ],
         ),
       ),
       body: Home(),
-
     );
   }
 }
-
 
 class Home extends StatefulWidget {
   @override
@@ -64,12 +106,10 @@ class _HomeState extends State<Home> {
             title: Text('Contact Us'),
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              title: Text('Products List')
-          ),
+              icon: Icon(Icons.person), title: Text('Products List')),
           BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              title: Text('Special Discounts'),
+            icon: Icon(Icons.person),
+            title: Text('Special Discounts'),
           )
         ],
       ),
